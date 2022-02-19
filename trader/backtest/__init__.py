b5294f3ee707fa17c2a25632277f1df416dc5ea3
Horvath Patrik <horypatrik98@gmail.com>
2022-02-19 22:40:24 +0100

    Created a separate class to plot backtest results instead of using a monolith function. 	- More customization to build the trade result figure. Implemented profit graph, to display individual trade profits/losses.

--- trader/backtest/__init__.py
+++ trader/backtest/__init__.py
@@ -6,7 +6,8 @@ from .position import BacktestPosition
 from .order_group import BacktestOrderGroup
 from .exception import NotEnoughFundsError
 from .backtester import run_backtest
-from .plot import Plot
+from .custom_graph import CustomGraph
+from .trade_figure import TradeResultFigure
 from .bot import BacktestBot, backtest_multiple_bot, BacktestRunParams
 from .balance import BacktestBalance
 from .indicator_optimizer import OptimizedIndicator
